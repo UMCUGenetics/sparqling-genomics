5404b3a47fa4e823374c05f087a278b074ed7d31
Roel Janssen <roel@gnu.org>
2017-08-11 15:49:38 +0200

    web: Add the query interface back-end.
    * web/Makefile.am: Adapt to new files.
    * web/static/css/main.css: Add styles for the results table.
    * web/static/datatables.min.js: Delete file.
    * web/static/jquery.dataTables.min.js: New file.
    * web/www/pages.scm: Load jQuery before DataTables.
    * web/www/pages/query-response.scm: New file.
    * web/www/pages/welcome.scm: Add bits for the query interface.

---
+++ web/www/pages/query-response.scm
@@ -0,0 +1,48 @@
+(define-module (www pages query-response)
+  #:use-module (www pages)
+  #:use-module (sparql driver)
+  #:use-module (web response)
+  #:use-module (ice-9 receive)
+  #:use-module (ice-9 rdelim)
+  #:use-module (srfi srfi-1)
+  #:use-module (sxml simple)
+
+  #:export (page-query-response))
+
+(define* (response->sxml port
+                         #:optional (read-header? #t)
+                         (header '())
+                         (body '()))
+  "Read the query response from PORT and turn it into a SXML table."
+  (let ((line (read-line port)))
+    (if (eof-object? line)
+        ;; When all data has been processed, we can assemble
+        ;; the table by wrapping the HEADER and BODY into a
+        ;; table construct.
+        `(table (@ (id "query-output"))
+                ,(cons 'thead header)
+                ,(cons 'tbody (reverse body)))
+        (let ((tokens (string-split line #\,)))
+          ;; The first line in the output is the table header.
+          (if read-header?
+              (response->sxml port #f
+                (append header
+                        `((tr ,(map (lambda (token)
+                                     `(th ,(string-trim-both token #\")))
+                                   tokens))))
+                body)
+              (response->sxml port #f header
+                (cons body
+                        `((tr ,(map (lambda (token)
+                                     `(td ,(string-trim-both token #\")))
+                                   tokens))))))))))
+
+(define* (page-query-response request-path #:key (post-data ""))
+  (if (string= post-data "")
+      '(p "Please send a POST request with a SPARQL query.")
+      (let ((result (receive (header port)
+                        (sparql-query post-data #:type "text/csv")
+                      (if (= (response-code header) 200)
+                          (response->sxml port)
+                          `(p "Oops, your query is invalid!")))))
+        result)))
--- web/www/pages/welcome.scm
+++ web/www/pages/welcome.scm
@@ -2,17 +2,27 @@
   #:use-module (www pages)
   #:export (page-welcome))
 
-(define (page-welcome request-path)
+(define* (page-welcome request-path #:key (post-data ""))
   (page-root-template "sparqling-svs" request-path
    `((h2 "Query the database")
+     (h3 "Query editor")
      (p "Use " (strong "Ctrl + Enter") " to execute the query.")
-     (div (@ (id "editor")) "PREFIX : <http://localhost:8890/TestGraph>
+     (div (@ (id "editor")) "PREFIX : <http://localhost:8890/TestGraph/>
 
-SELECT ?variant
+SELECT ?variant ?chromosome ?position ?filter
 WHERE {
-  ?variant a :Variant
+  ?variant a :SNPVariant .
+  ?variant :genome_position ?p .
+  ?variant :filter ?filter .
+  ?p :chromosome ?chromosome .
+  ?p :position ?position .
+
+  FILTER (?chromosome = \"1\")
+  FILTER (?position > 1000000)
+  FILTER (?position < 9000000)
 }
-ORDER BY ?variant")
+LIMIT 500
+")
      (script "
 $(document).ready(function(){
   var editor = ace.edit('editor');
@@ -23,13 +33,13 @@ $(document).ready(function(){
   session.setTabSize(2);
 
   /* Set the editor to the maximum height within the content area. */
-  var page_height = $(document).height();
-  var content_pos = $('#content').position();
-  var editor_pos = $('#editor').position();
-  var editor_height = page_height - 85 - editor_pos.top;
-  $('#wrapper').height(page_height);
-  $('#content').height(page_height - content_pos.top - 100);
-  $('#editor').height(editor_height);
+  //var page_height = $(document).height();
+  //var content_pos = $('#content').position();
+  //var editor_pos = $('#editor').position();
+  //var editor_height = page_height - 85 - editor_pos.top;
+  //$('#wrapper').height(page_height);
+  //$('#content').height(page_height - content_pos.top - 100);
+  //$('#editor').height(editor_height);
 
   /* Add keybindings for copying the text and for running the query. */
   editor.commands.addCommand({
@@ -38,10 +48,11 @@ $(document).ready(function(){
     exec: function(editor) {
       $('#content').after('" (textarea (@ (id "copyText"))) "');
       var temp = document.getElementById('copyText');
-      temp.value = editor.getValue();
+      temp.value = editor.getSelectedText();
       temp.select();
       document.execCommand('copy');
       temp.remove();
+      $('.ace_text-input').focus();
       }, readOnly: true
     });
 
@@ -49,9 +60,24 @@ $(document).ready(function(){
     name: 'executeQueryCommand',
     bindKey: {win: 'Ctrl-Enter',  mac: 'Command-Enter'},
     exec: function(editor) {
-      alert('Sorry.  The actual query functionality is still missing..');
+      $.post('/query-response', editor.getValue(), function (data){
+
+        /* Remove the previous query results. */
+        $('#query-results').remove();
+        $('#query-output').remove();
+        $('#query-output_wrapper').remove();
+
+        /*  Insert the results HTML table into the page. */
+        $('#editor').after(data);
+        $('#editor').after(function(){ return '" (h3 (@ (id "query-results")) "Query results") "' });
+
+        /* Initialize DataTables. */
+        $('#query-output').addClass('display');
+        var dt = $('#query-output').DataTable({ sDom: 'lrtip' });
+        dt.draw();
+      });
       }, readOnly: true
     });
 });
 "))
-   #:dependencies '(ace jquery)))
+   #:dependencies '(ace jquery datatables)))
