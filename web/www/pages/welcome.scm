a4b932bec09415b4632233be4727efe3dac13799
Roel Janssen <roel@gnu.org>
2017-11-20 11:13:23 +0100

    web: Show non-distinct number of variants.
    * web/static/css/main.css: Small stylesheet tweaks.
    * web/www/pages/welcome.scm: Implement another SPARQL query.

--- web/www/pages/welcome.scm
+++ web/www/pages/welcome.scm
@@ -11,9 +11,21 @@
 
 (define %db-statistics-query
   "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX : <http://localhost:5000/MyrtheLivers/>
+PREFIX : <http://localhost:5000/StructuralVariants/>
 
 SELECT COUNT(DISTINCT ?filename) AS ?numberOfFiles, COUNT(DISTINCT ?variant) AS ?numberOfVariants
+{
+  ?origin a :Origin .
+  ?origin :filename ?filename .
+  ?variant a :Variant .
+  ?variant :origin ?origin .
+}")
+
+(define %db-count-query
+  "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX : <http://localhost:5000/StructuralVariants/>
+
+SELECT ?filename, COUNT(DISTINCT ?variant) AS ?numberOfVariants
 {
   ?origin a :Origin .
   ?origin :filename ?filename .
@@ -34,14 +46,40 @@ SELECT COUNT(DISTINCT ?filename) AS ?numberOfFiles, COUNT(DISTINCT ?variant) AS 
     (lambda (key . args)
       '())))
 
+(define* (process-count-results port #:optional (results '()))
+  (let ((line (read-line port)))
+    (if (eof-object? line)
+        results
+        (process-count-results port
+                               (cons (string->number
+                                      (cadr (csv-split-line line #\,)))
+                                     results)))))
+
+(define (variant-count)
+  (catch 'system-error
+    (lambda _
+      (receive (header port) (sparql-query %db-count-query #:type "text/csv")
+        (if (= (response-code header) 200)
+            (begin
+              ;; The first line is the header.
+              (read-line port)
+              (apply + (process-count-results port)))
+            0)))
+    (lambda (key . args) 0)))
+
+(variant-count)
+
 (define* (page-welcome request-path #:key (post-data ""))
   (let ((plot-file (string-append %www-static-root
                                   "/images/mutations-per-type.svg"))
         (external (string-append "/static/images/mutations-per-type.svg"))
-        (statistics (database-statistics)))
+        (statistics (database-statistics))
+        (validation-count (variant-count)))
     (page-root-template "sparqling-svs" request-path
      `((h2 "Overview")
        ,(if (null? statistics)
             `(p "There is a problem connecting to the database.")
-            `(p "The database contains " ,(car statistics) " files, with "
-                ,(cadr statistics) " variants."))))))
+            `(p "The database contains " ,(car statistics)
+                ,(if (= (car statistics) 1) " file" " files") " and "
+                ,validation-count " variants, of which " ,(cadr statistics)
+                " are distinct."))))))
