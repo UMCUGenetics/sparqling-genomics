UMCUGenetics/sparqling-genomics

commit 57e1d4e71939819d4097cf6dd0df1128f2398ba6

table2rdf: Implement predicate transformers.
* tools/table2rdf/include/runtime_configuration.h: Add state variables for
  predicate transformers.
* tools/table2rdf/include/table.h: Likewise.
* tools/table2rdf/src/runtime_configuration.c: Split transformer initializers
  into 'predicate' and 'object' transformers.
* tools/table2rdf/src/table.c: Implement predicate transformers.
* tools/table2rdf/src/ui.c: Add -T to command-line arguments.

tools/table2rdf/src/table.c
@@ -83,11 +83,24 @@ process_header (FILE* stream, const unsigned char *origin, const char *filename)
 
   header->keys = calloc (64, sizeof (char *));
   header->column_ids = calloc (64, sizeof (char *));
+  header->object_transformer_ids = calloc (64, sizeof (int32_t *));
+  header->predicate_transformer_ids = calloc (64, sizeof (int32_t *));
+
   header->keys_alloc_len = 64;
-  if (header->keys == NULL || header->column_ids == NULL)
+  if (header->keys == NULL
+      || header->column_ids == NULL
+      || header->object_transformer_ids == NULL
+      || header->predicate_transformer_ids == NULL)
     {
       ui_print_general_memory_error();
       return NULL;
+    }
+
+  int32_t index;
+  for (index = 0; index < 64; index++)
+    {
+      header->object_transformer_ids[index] = TRANSFORMER_INDEX_UNKNOWN;
+      header->predicate_transformer_ids[index] = TRANSFORMER_INDEX_UNKNOWN;
     }
 
   char *line = NULL;
@@ -127,11 +140,28 @@ process_header (FILE* stream, const unsigned char *origin, const char *filename)
                                       header->keys_alloc_len * sizeof (char *));
               header->column_ids = realloc (header->column_ids,
                                             header->keys_alloc_len * sizeof (char *));
+              header->object_transformer_ids = realloc (header->object_transformer_ids,
+                                                        header->keys_alloc_len *
+                                                        sizeof (int32_t *));
+              header->predicate_transformer_ids = realloc (header->predicate_transformer_ids,
+                                                           header->keys_alloc_len *
+                                                           sizeof (int32_t *));
 
-              if (header->keys == NULL || header->column_ids == NULL)
+              if (header->keys == NULL
+                  || header->column_ids == NULL
+                  || header->object_transformer_ids == NULL
+                  || header->predicate_transformer_ids == NULL)
                 {
                   ui_print_general_memory_error();
                   return NULL;
+                }
+
+              for (index = header->keys_alloc_len - 64; // The old length
+                   index < header->keys_alloc_len;
+                   index++)
+                {
+                  header->object_transformer_ids[index] = TRANSFORMER_INDEX_UNKNOWN;
+                  header->predicate_transformer_ids[index] = TRANSFORMER_INDEX_UNKNOWN;
                 }
             }
 
@@ -201,6 +231,7 @@ process_column (table_hdr_t* hdr, char *token, uint32_t column_index)
   uint32_t trimmed_length = 0;
   char *trimmed_token     = NULL;
   raptor_statement *stmt  = NULL;
+  int32_t trans_index     = 0;
 
   trimmed_token = trim_quotes (token, strlen (token));
   if (trimmed_token == NULL) return;
@@ -208,23 +239,61 @@ process_column (table_hdr_t* hdr, char *token, uint32_t column_index)
 
   stmt = raptor_new_statement (config.raptor_world);
   stmt->subject   = term (PREFIX_ROW, config.id_buf);
-  stmt->predicate = term (PREFIX_COLUMN, hdr->column_ids[column_index]);
 
-  int32_t trans_index = 0;
-  for (; trans_index < config.transformer_len; trans_index++)
-    if (!strcmp (hdr->column_ids[column_index],
-                 config.transformer_keys[trans_index]))
-      break;
+  /* ------------------------------------------------------------------------
+   * PREDICATE TRANSFORMATION
+   * ------------------------------------------------------------------------ */
 
-  if (trans_index < config.transformer_len)
+  trans_index = hdr->predicate_transformer_ids[column_index];
+  if (trans_index == TRANSFORMER_INDEX_UNKNOWN)
+    {
+      trans_index = 0;
+      for (; trans_index < config.predicate_transformer_len; trans_index++)
+        if (!strcmp (hdr->column_ids[column_index],
+                     config.predicate_transformer_keys[trans_index]))
+          break;
+
+      if (trans_index >= config.predicate_transformer_len)
+        trans_index = TRANSFORMER_INDEX_UNAVAILABLE;
+
+      hdr->predicate_transformer_ids[column_index] = trans_index;
+    }
+
+  if (trans_index >= 0 && trans_index < config.predicate_transformer_len)
+    stmt->predicate = raptor_new_term_from_uri_string (config.raptor_world,
+                                                       config.predicate_transformer_values[trans_index]);
+  else
+    stmt->predicate = term (PREFIX_COLUMN, hdr->column_ids[column_index]);
+
+  /* ------------------------------------------------------------------------
+   * OBJECT TRANSFORMATION
+   * ------------------------------------------------------------------------ */
+
+  trans_index = hdr->object_transformer_ids[column_index];
+  if (trans_index == TRANSFORMER_INDEX_UNKNOWN)
+    {
+      trans_index = 0;
+      for (; trans_index < config.object_transformer_len; trans_index++)
+        if (!strcmp (hdr->column_ids[column_index],
+                     config.object_transformer_keys[trans_index]))
+          break;
+
+      if (trans_index >= config.object_transformer_len)
+        trans_index = TRANSFORMER_INDEX_UNAVAILABLE;
+
+      hdr->object_transformer_ids[column_index] = trans_index;
+    }
+
+  /* When a transformer is available, we treat the value as a URI. */
+  if (trans_index >= 0 && trans_index < config.object_transformer_len)
     {
       /* The ontology can either use a '/' or a '#' as separator.
        * In Redland, an '#' behaves different than a '/'.  We have
        * to deal with that here. */
       char *end_token = trimmed_token;
       bool end_token_allocated = false;
-      uint32_t uri_len = strlen (config.transformer_values[trans_index]);
-      if (config.transformer_values[trans_index][uri_len - 1] == '#')
+      uint32_t uri_len = strlen (config.object_transformer_values[trans_index]);
+      if (config.object_transformer_values[trans_index][uri_len - 1] == '#')
         {
           uint32_t token_len = strlen (trimmed_token);
           end_token = calloc (token_len + 2, sizeof (char));
@@ -246,6 +315,9 @@ process_column (table_hdr_t* hdr, char *token, uint32_t column_index)
       if (end_token_allocated)
         free (end_token);
     }
+
+  /* Without a transformer, the value will be treated as a "literal" instead
+   * of a URI. */
   else
     {
       /* Determine the actual type this data represents.

tools/table2rdf/include/runtime_configuration.h
@@ -43,13 +43,20 @@ typedef struct
   char              *secondary_delimiter;
   char              *header_line;
   char              *sample_name;
-  char              **transformers_buffer;
-  char              **transformer_keys;
-  char              **transformer_values;
-  uint32_t          transformers_buffer_len;
-  uint32_t          transformers_buffer_alloc_len;
-  uint32_t          transformer_alloc_len;
-  uint32_t          transformer_len;
+  char              **predicate_transformers_buffer;
+  char              **predicate_transformer_keys;
+  char              **predicate_transformer_values;
+  char              **object_transformers_buffer;
+  char              **object_transformer_keys;
+  char              **object_transformer_values;
+  uint32_t          predicate_transformers_buffer_len;
+  uint32_t          predicate_transformers_buffer_alloc_len;
+  uint32_t          predicate_transformer_alloc_len;
+  uint32_t          predicate_transformer_len;
+  uint32_t          object_transformers_buffer_len;
+  uint32_t          object_transformers_buffer_alloc_len;
+  uint32_t          object_transformer_alloc_len;
+  uint32_t          object_transformer_len;
   bool              show_progress_info;
   bool              skip_first_line;
   bool              input_from_stdin;
@@ -82,7 +89,9 @@ void runtime_configuration_redland_free (void);
 bool generate_column_id (const unsigned char *origin, char *column_id);
 bool generate_row_id (const unsigned char *origin, char *row_id);
 bool generate_prefix_name (unsigned char *prefix_name);
-bool preregister_transformer (const char *pair);
-bool register_transformers ();
+bool preregister_predicate_transformer (const char *pair);
+bool preregister_object_transformer (const char *pair);
+bool register_predicate_transformers ();
+bool register_object_transformers ();
 
 #endif  /* RUNTIMECONFIGURATION_H */

tools/table2rdf/include/table.h
@@ -21,10 +21,15 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define TRANSFORMER_INDEX_UNKNOWN      -2
+#define TRANSFORMER_INDEX_UNAVAILABLE  -1
+
 typedef struct {
   char *header_line;
   char **column_ids;
   char **keys;
+  int32_t *predicate_transformer_ids;
+  int32_t *object_transformer_ids;
   uint32_t keys_len;
   uint32_t keys_alloc_len;
 } table_hdr_t;

tools/table2rdf/src/runtime_configuration.c
@@ -33,13 +33,20 @@ runtime_configuration_init (void)
   config.header_line = NULL;
   config.sample_name = NULL;
   config.output_format = NULL;
-  config.transformers_buffer = NULL;
-  config.transformer_keys = NULL;
-  config.transformer_values = NULL;
-  config.transformers_buffer_len = 0;
-  config.transformer_len = 0;
-  config.transformers_buffer_alloc_len = 0;
-  config.transformer_alloc_len = 0;
+  config.object_transformers_buffer = NULL;
+  config.object_transformer_keys = NULL;
+  config.object_transformer_values = NULL;
+  config.object_transformers_buffer_len = 0;
+  config.object_transformer_len = 0;
+  config.object_transformers_buffer_alloc_len = 0;
+  config.object_transformer_alloc_len = 0;
+  config.predicate_transformers_buffer = NULL;
+  config.predicate_transformer_keys = NULL;
+  config.predicate_transformer_values = NULL;
+  config.predicate_transformers_buffer_len = 0;
+  config.predicate_transformer_len = 0;
+  config.predicate_transformers_buffer_alloc_len = 0;
+  config.predicate_transformer_alloc_len = 0;
   config.column_counter = 0;
   config.row_counter = 0;
   config.prefix_name_counter = 0;
@@ -67,7 +74,10 @@ runtime_configuration_redland_init (void)
   if (!ontology_init (&(config.ontology)))
     return (ui_print_redland_error () == 0);
 
-  if (!register_transformers ())
+  if (!register_object_transformers ())
+    return (ui_print_redland_error () == 0);
+
+  if (!register_predicate_transformers ())
     return (ui_print_redland_error () == 0);
 
   return true;
@@ -80,20 +90,36 @@ runtime_configuration_redland_free (void)
   ontology_free (config.ontology);
 
   uint32_t index = 0;
-  for (; index < config.transformer_len; index++)
+  for (; index < config.object_transformer_len; index++)
     {
-      /* We do not have to clean up transformer_values,
+      /* We do not have to clean up 'object_transformer_values',
        * because it was allocated in one go. */
-      free (config.transformer_keys[index]);
+      free (config.object_transformer_keys[index]);
     }
 
-  free (config.transformer_keys);
-  free (config.transformer_values);
+  free (config.object_transformer_keys);
+  free (config.object_transformer_values);
 
-  for (index = 0; index < config.transformers_buffer_len; index++)
-    free (config.transformers_buffer[index]);
+  for (index = 0; index < config.object_transformers_buffer_len; index++)
+    free (config.object_transformers_buffer[index]);
 
-  free (config.transformers_buffer);
+  free (config.object_transformers_buffer);
+
+  for (index = 0; index < config.predicate_transformer_len; index++)
+    {
+      /* We do not have to clean up 'predicate_transformer_values',
+       * because it was allocated in one go. */
+      free (config.predicate_transformer_keys[index]);
+    }
+
+  free (config.predicate_transformer_keys);
+  free (config.predicate_transformer_values);
+
+  for (index = 0; index < config.predicate_transformers_buffer_len; index++)
+    free (config.predicate_transformers_buffer[index]);
+
+  free (config.predicate_transformers_buffer);
+
 }
 
 void
@@ -142,40 +168,40 @@ generate_prefix_name (unsigned char *prefix_name)
 }
 
 bool
-preregister_transformer (const char *pair)
+preregister_object_transformer (const char *pair)
 {
   /* Always make sure there are enough indexes. */
-  if (config.transformers_buffer_alloc_len == 0 ||
-      config.transformers_buffer_len == config.transformers_buffer_alloc_len - 1)
+  if (config.object_transformers_buffer_alloc_len == 0 ||
+      config.object_transformers_buffer_len == config.object_transformers_buffer_alloc_len - 1)
     {
-      char **buffer = realloc (config.transformers_buffer,
-                               config.transformers_buffer_alloc_len +
+      char **buffer = realloc (config.object_transformers_buffer,
+                               config.object_transformers_buffer_alloc_len +
                                sizeof (char *) * 32);
       if (buffer == NULL) return false;
-      config.transformers_buffer = buffer;
+      config.object_transformers_buffer = buffer;
     }
 
   char *duplicate = strdup (pair);
   if (duplicate == NULL)
     return false;
 
-  config.transformers_buffer[config.transformers_buffer_len] = duplicate;
-  config.transformers_buffer_len += 1;
+  config.object_transformers_buffer[config.object_transformers_buffer_len] = duplicate;
+  config.object_transformers_buffer_len += 1;
 
   return true;
 }
 
 bool
-register_transformers ()
+register_object_transformers ()
 {
   uint32_t index = 0;
-  for (; index < config.transformers_buffer_len; index++)
+  for (; index < config.object_transformers_buffer_len; index++)
     {
-      char *trans = config.transformers_buffer[index];
+      char *trans = config.object_transformers_buffer[index];
       char *separator = strchr (trans, '=');
       if (separator == NULL)
         {
-          fprintf (stderr, "Warning: Ignoring invalid transformer separator: '%s'\n", trans);
+          fprintf (stderr, "Warning: Ignoring invalid object_transformer separator: '%s'\n", trans);
           continue;
         }
 
@@ -183,24 +209,24 @@ register_transformers ()
       *separator = '\0';
 
       /* Always make sure there are enough indexes. */
-      if (config.transformer_alloc_len == 0 ||
-          config.transformer_len == config.transformer_alloc_len - 1)
+      if (config.object_transformer_alloc_len == 0 ||
+          config.object_transformer_len == config.object_transformer_alloc_len - 1)
         {
-          char **keys = realloc (config.transformer_keys,
-                                 config.transformer_alloc_len +
+          char **keys = realloc (config.object_transformer_keys,
+                                 config.object_transformer_alloc_len +
                                  sizeof (char *) * 32);
           if (keys == NULL) return false;
-          config.transformer_keys = keys;
+          config.object_transformer_keys = keys;
 
-          char **values = realloc (config.transformer_values,
-                                   config.transformer_alloc_len +
+          char **values = realloc (config.object_transformer_values,
+                                   config.object_transformer_alloc_len +
                                    sizeof (char *) * 32);
 
           if (values == NULL) return false;
-          config.transformer_values = values;
+          config.object_transformer_values = values;
 
-          if (config.transformer_keys == NULL ||
-              config.transformer_values == NULL)
+          if (config.object_transformer_keys == NULL ||
+              config.object_transformer_values == NULL)
             return false;
         }
 
@@ -219,10 +245,10 @@ register_transformers ()
       if (!generate_prefix_name (prefix_name))
         return false;
 
-      config.transformer_keys[config.transformer_len] =
+      config.object_transformer_keys[config.object_transformer_len] =
         sanitize_string (trans, ((separator - trans) * sizeof (char)));
 
-      config.transformer_values[config.transformer_len] = value;
+      config.object_transformer_values[config.object_transformer_len] = value;
       config.ontology->prefixes[config.ontology->prefixes_length - 1] =
         raptor_new_uri (config.raptor_world, (unsigned char *)value);
 
@@ -230,8 +256,105 @@ register_transformers ()
                                        config.ontology->prefixes[config.ontology->prefixes_length - 1],
                                        prefix_name);
 
-      config.transformer_len += 1;
+      config.object_transformer_len += 1;
     }
 
   return true;
 }
+
+bool
+preregister_predicate_transformer (const char *pair)
+{
+  /* Always make sure there are enough indexes. */
+  if (config.predicate_transformers_buffer_alloc_len == 0 ||
+      config.predicate_transformers_buffer_len == config.predicate_transformers_buffer_alloc_len - 1)
+    {
+      char **buffer = realloc (config.predicate_transformers_buffer,
+                               config.predicate_transformers_buffer_alloc_len +
+                               sizeof (char *) * 32);
+      if (buffer == NULL) return false;
+      config.predicate_transformers_buffer = buffer;
+    }
+
+  char *duplicate = strdup (pair);
+  if (duplicate == NULL)
+    return false;
+
+  config.predicate_transformers_buffer[config.predicate_transformers_buffer_len] = duplicate;
+  config.predicate_transformers_buffer_len += 1;
+
+  return true;
+}
+
+bool
+register_predicate_transformers ()
+{
+  uint32_t index = 0;
+  for (; index < config.predicate_transformers_buffer_len; index++)
+    {
+      char *trans = config.predicate_transformers_buffer[index];
+      char *separator = strchr (trans, '=');
+      if (separator == NULL)
+        {
+          fprintf (stderr, "Warning: Ignoring invalid predicate_transformer separator: '%s'\n", trans);
+          continue;
+        }
+
+      char *value = separator + sizeof (char);
+      *separator = '\0';
+
+      /* Always make sure there are enough indexes. */
+      if (config.predicate_transformer_alloc_len == 0 ||
+          config.predicate_transformer_len == config.predicate_transformer_alloc_len - 1)
+        {
+          char **keys = realloc (config.predicate_transformer_keys,
+                                 config.predicate_transformer_alloc_len +
+                                 sizeof (char *) * 32);
+
+          if (keys == NULL) return false;
+          config.predicate_transformer_keys = keys;
+
+          char **values = realloc (config.predicate_transformer_values,
+                                   config.predicate_transformer_alloc_len +
+                                   sizeof (char *) * 32);
+
+          if (values == NULL) return false;
+          config.predicate_transformer_values = values;
+
+          if (config.predicate_transformer_keys == NULL ||
+              config.predicate_transformer_values == NULL)
+            return false;
+        }
+
+      config.ontology->prefixes_length += 1;
+      raptor_uri **temp = realloc (config.ontology->prefixes,
+                                   config.ontology->prefixes_length * sizeof (raptor_uri*));
+      if (temp == NULL)
+        {
+          config.ontology->prefixes_length -= 1;
+          return false;
+        }
+
+      config.ontology->prefixes = temp;
+
+      unsigned char prefix_name[12];
+      if (!generate_prefix_name (prefix_name))
+        return false;
+
+      config.predicate_transformer_keys[config.predicate_transformer_len] =
+        sanitize_string (trans, ((separator - trans) * sizeof (char)));
+
+      config.predicate_transformer_values[config.predicate_transformer_len] = value;
+      config.ontology->prefixes[config.ontology->prefixes_length - 1] =
+        raptor_new_uri (config.raptor_world, (unsigned char *)value);
+
+      raptor_serializer_set_namespace (config.raptor_serializer,
+                                       config.ontology->prefixes[config.ontology->prefixes_length - 1],
+                                       prefix_name);
+
+      config.predicate_transformer_len += 1;
+    }
+
+  return true;
+}
+

tools/table2rdf/src/ui.c
@@ -29,33 +29,38 @@ void
 ui_show_help (void)
 {
   puts ("\nAvailable options:\n"
-	"  --help,                  -h  Show this message.\n"
-	"  --progress-info,         -p  Show progress information.\n"
-	"  --version,               -v  Show versioning information.\n"
-        "  --caller=ARG,            -c  The program used to produce the input "
-                                       "file.\n"
-        "  --sample-name            -s  The name of the sample to which the "
-                                       "data belongs.\n"
-	"  --delimiter,             -d  The delimiter to distinguish fields "
-                                       "in the file.\n"
-	"  --secondary-delimiter,   -D  A secondary delimiter to distinguish "
-                                       "fields in a field.\n"
-	"  --header-line,           -H  When the input file does not contain a "
-                                       "header\n"
-        "                               line, provide it here. When using this "
-                                       "argument,\n"
-        "                               the header line must use ';' as the "
-                                       "delimiter.\n"
-        "  --skip-first-line=ARG,   -S  Ignore the first line in the file.\n"
-        "  --transform=ARG          -t  A pair in the form colname=uri-prefix "
-                                       "where the column identified by colname "
-                                       "is transformed into a URI for which "
-                                       "uri-prefix is the prefix.  This argument "
-                                       "may be repeated.\n"
-        "  --input-file=ARG,        -i  The input file to process.\n"
-        "  --stdin,                 -I  Read input from a pipe instead of a "
-                                       "file.\n"
-        "  --output-format          -O  The output format to serialize to.\n");
+	"  --help,                   -h  Show this message.\n"
+	"  --progress-info,          -p  Show progress information.\n"
+	"  --version,                -v  Show versioning information.\n"
+        "  --caller=ARG,             -c  The program used to produce the input "
+                                        "file.\n"
+        "  --sample-name             -s  The name of the sample to which the "
+                                        "data belongs.\n"
+	"  --delimiter,              -d  The delimiter to distinguish fields "
+                                        "in the file.\n"
+	"  --secondary-delimiter,    -D  A secondary delimiter to distinguish "
+                                        "fields in a field.\n"
+	"  --header-line,            -H  When the input file does not contain a "
+                                        "header\n"
+        "                                line, provide it here. When using this "
+                                        "argument,\n"
+        "                                the header line must use ';' as the "
+                                        "delimiter.\n"
+        "  --skip-first-line=ARG,    -S  Ignore the first line in the file.\n"
+        "  --transform-object=ARG    -t  A pair in the form colname=uri-prefix "
+                                        "where the value\n"
+        "                                for the column identified by colname "
+                                        "is transformed\n"
+        "                                into a URI for "
+                                        "which uri-prefix is the prefix.  This"
+        "\n                                argument may be repeated.\n"
+        "  --transform-predicate=ARG -T  Same as -t, except this operates on the "
+                                        "column names\n"
+        "                                instead of the values.\n"
+        "  --input-file=ARG,         -i  The input file to process.\n"
+        "  --stdin,                  -I  Read input from a pipe instead of a "
+                                        "file.\n"
+        "  --output-format           -O  The output format to serialize to.\n");
 }
 
 void
@@ -86,7 +91,8 @@ ui_process_command_line (int argc, char **argv)
       { "output-format",         required_argument, 0, 'O' },
       { "progress-info",         no_argument,       0, 'p' },
       { "skip-first-line",       no_argument,       0, 'S' },
-      { "transform",             required_argument, 0, 't' },
+      { "transform-object",      required_argument, 0, 't' },
+      { "transform-predicate",   required_argument, 0, 'T' },
       { "version",               no_argument,       0, 'v' },
       { 0,                       0,                 0, 0   }
     };
@@ -94,7 +100,7 @@ ui_process_command_line (int argc, char **argv)
   while ( arg != -1 )
     {
       /* Make sure to list all short options in the string below. */
-      arg = getopt_long (argc, argv, "c:d:D:i:O:H:s:St:Iophv", options, &index);
+      arg = getopt_long (argc, argv, "c:d:D:i:O:H:s:St:T:Iophv", options, &index);
       switch (arg)
         {
         case 'c': config.caller = optarg;                        break;
@@ -107,7 +113,8 @@ ui_process_command_line (int argc, char **argv)
         case 'H': config.header_line = optarg;                   break;
         case 's': config.sample_name = optarg;                   break;
         case 'S': config.skip_first_line = true;                 break;
-        case 't': preregister_transformer (optarg);              break;
+        case 't': preregister_object_transformer (optarg);       break;
+        case 'T': preregister_predicate_transformer (optarg);    break;
         case 'h': ui_show_help ();                               break;
         case 'v': ui_show_version ();                            break;
         }

commit 21a612f446392b0b0f8680b870a68a99b61a32f0

table2rdf: Fix memory leaks introduced in 57e1d4e.
* configure.ac: Adhere to 80-columns-per-line.
* tools/table2rdf/src/main.c: Free memory for the transformer_ids.
* tools/table2rdf/src/table.c: Explicitly cast unsigned char to char.

configure.ac
@@ -131,9 +131,10 @@ fi
 
 AC_SUBST(ENABLE_MTRACE_OPTION)
 AC_ARG_ENABLE([mtrace],
-AC_HELP_STRING([--enable-mtrace],
-[enable mtrace support @<:@default=no@:>@]),
-AM_CONDITIONAL([ENABLE_MTRACE_OPTION], [true]), AM_CONDITIONAL([ENABLE_MTRACE_OPTION], [false]))
+  AC_HELP_STRING([--enable-mtrace],
+  [enable mtrace support @<:@default=no@:>@]),
+  AM_CONDITIONAL([ENABLE_MTRACE_OPTION], [true]),
+  AM_CONDITIONAL([ENABLE_MTRACE_OPTION], [false]))
 
 AM_CONDITIONAL([HAVE_PDFLATEX], test -n "$PDFLATEX")
 AM_CONDITIONAL([HAVE_HTLATEX], test -n "$HTLATEX")

tools/table2rdf/src/main.c
@@ -205,6 +205,8 @@ main (int argc, char **argv)
 
       free (table->keys);
       free (table->column_ids);
+      free (table->object_transformer_ids);
+      free (table->predicate_transformer_ids);
       free (table);
 
       /* Clean up. */

tools/table2rdf/src/table.c
@@ -261,7 +261,8 @@ process_column (table_hdr_t* hdr, char *token, uint32_t column_index)
 
   if (trans_index >= 0 && trans_index < config.predicate_transformer_len)
     stmt->predicate = raptor_new_term_from_uri_string (config.raptor_world,
-                                                       config.predicate_transformer_values[trans_index]);
+                                                       ((unsigned char *)
+                                                        config.predicate_transformer_values[trans_index]));
   else
     stmt->predicate = term (PREFIX_COLUMN, hdr->column_ids[column_index]);
 