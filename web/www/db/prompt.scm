64a94da4987c6873e37ef8bee63d9028bcc63aa8
Roel Janssen <roel@gnu.org>
2019-05-01 15:52:34 +0200

    web: Add missing file.
    * web/www/db/prompt.scm: New file.
    * web/Makefile.am: Register ‘www/db/prompt.scm’.

---
+++ web/www/db/prompt.scm
@@ -0,0 +1,188 @@
+;;; Copyright © 2019  Roel Janssen <roel@gnu.org>
+;;;
+;;; This program is free software: you can redistribute it and/or
+;;; modify it under the terms of the GNU Affero General Public License
+;;; as published by the Free Software Foundation, either version 3 of
+;;; the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Affero General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Affero General Public
+;;; License along with this program.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+
+(define-module (www db prompt)
+  #:use-module (ice-9 receive)
+  #:use-module (sparql driver)
+  #:use-module (sparql util)
+  #:use-module (srfi srfi-1)
+  #:use-module (web response)
+  #:use-module (www config)
+  #:use-module (www db connections)
+  #:use-module (www db exploratory)
+  #:use-module (www db sessions)
+  #:use-module (www util)
+
+  #:export (prompt-insert-triplet
+            prompt-remove-triplet
+            prompt-get-triplets
+            prompt-clear-triplets
+            prompt-save-session))
+
+(define (restore-uri input)
+  (cond
+   [(is-uri? input)
+    (if (string-prefix? "<" input)
+        input
+        (string-append "<" input ">"))]
+   [(is-shorthand-uri? input)
+    input]
+   [(string->number input)
+    input]
+   [else
+    (if (string-prefix? "\"" input)
+        input
+        (string-append "\"" input "\""))]))
+
+;; PROMPT-INSERT-TRIPLET
+;; ----------------------------------------------------------------------------
+;;
+;; This function stores a value into the cache graph.
+;;
+
+(define (prompt-insert-triplet username subject predicate object)
+  (let [(connection (default-connection username))]
+    (catch #t
+      (lambda _
+        (let [(query (format #f "~a
+INSERT INTO <http://~a/sg-prompt> {
+  ~a ~a ~a
+}" default-prefixes username
+ (restore-uri subject)
+ (restore-uri predicate)
+ (restore-uri object)))]
+          (receive (header port)
+              (sparql-query query
+                            #:uri (connection-uri connection)
+                            #:store-backend
+                            (connection-backend connection)
+                            #:digest-auth
+                            (if (and (connection-username connection)
+                                     (connection-password connection))
+                                (string-append
+                                 (connection-username connection) ":"
+                                 (connection-password connection))
+                                #f))
+            (= (response-code header) 200))))
+      (lambda (key . args)
+        (format #t "Unknown exception thrown in ~a: ~a: ~a~%"
+                "prompt-insert-triplet" key args)))))
+
+;; PROMPT-REMOVE-TRIPLET
+;; ----------------------------------------------------------------------------
+;;
+;; This function removes a triplet from the prompt session's graph.
+;;
+
+(define (prompt-remove-triplet username subject predicate object)
+  (let [(connection (default-connection username))]
+    (catch #t
+      (lambda _
+        (let [(query (format #f "~a
+DELETE DATA {
+  GRAPH <http://~a/sg-prompt> {
+    ~a ~a ~a
+  }
+}
+" default-prefixes
+  username
+  (restore-uri subject)
+  (restore-uri predicate)
+  (restore-uri object)))]
+          (receive (header port)
+              (user-sparql-query username query)
+            (= (response-code header) 200))))
+      (lambda (key . args)
+        (format #t "Unknown exception thrown in ~a: ~a: ~a~%"
+                "prompt-remove-triplet" key args)))))
+
+;; PROMPT-GET-TRIPLETS
+;; ----------------------------------------------------------------------------
+
+(define (prompt-get-triplets username)
+  (let [(connection (default-connection username))]
+    (catch #t
+      (lambda _
+        (let [(results
+               (query-results->list
+                (sparql-query
+                 (format #f "SELECT DISTINCT ?s ?p ?o WHERE { GRAPH <~a> { ?s ?p ?o } }"
+                         (string-append "http://" username "/sg-prompt"))
+                 #:uri           (connection-uri connection)
+                 #:store-backend (connection-backend connection)
+                 #:digest-auth   (if (and (connection-username connection)
+                                          (connection-password connection))
+                                     (string-append
+                                      (connection-username connection) ":"
+                                      (connection-password connection))
+                                     #f))
+                #t))]
+          (if results
+              results
+              '())))
+      (lambda (key . args)
+        (format #t "Unknown exception thrown in ~a: ~a: ~a~%"
+                "prompt-get-triplets" key args)
+        '()))))
+
+;; PROMPT-CLEAR-TRIPLETS
+;; ----------------------------------------------------------------------------
+
+(define (prompt-clear-triplets username)
+  (let [(connection (default-connection username))]
+    (catch #t
+      (lambda _
+        (query-results->list
+         (sparql-query
+          (string-append "CLEAR GRAPH <http://" username "/sg-prompt>")
+          #:uri           (connection-uri connection)
+          #:store-backend (connection-backend connection)
+          #:digest-auth   (if (and (connection-username connection)
+                                   (connection-password connection))
+                              (string-append
+                               (connection-username connection) ":"
+                               (connection-password connection))
+                              #f))
+         #t))
+      (lambda (key . args)
+        (format #t "Unknown exception thrown in ~a: ~a: ~a~%"
+                "prompt-clear-triplets" key args)
+        '()))))
+
+(define (prompt-save-session username graph)
+  (let [(connection (default-connection username))]
+    (catch #t
+      (lambda _
+        (receive (header port)
+            (sparql-query
+             (string-append "INSERT { GRAPH <" graph "> { ?s ?p ?o } } "
+                            "WHERE { GRAPH <http://" username "/sg-prompt> { "
+                            "?s ?p ?o } }")
+             #:uri           (connection-uri connection)
+             #:store-backend (connection-backend connection)
+             #:digest-auth   (if (and (connection-username connection)
+                                      (connection-password connection))
+                                 (string-append
+                                  (connection-username connection) ":"
+                                  (connection-password connection))
+                                 #f))
+          (if (= (response-code header) 200)
+              (prompt-clear-triplets username)
+              #f)))
+      (lambda (key . args)
+        (format #t "Unknown exception thrown in ~a: ~a: ~a~%"
+                "prompt-save-session" key args)
+        '()))))
