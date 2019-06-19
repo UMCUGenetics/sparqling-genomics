4f142578e47bbdc199ee8f5a9c9b6af1d5114f1a
Roel Janssen <roel@gnu.org>
2019-06-19 15:21:15 +0200

    Add json2rdf.
    * Makefile.am: Register ‘tools/json2rdf’.
    * configure.ac: Likewise.
    * tools/json2rdf/Makefile.am: New file.
    * tools/json2rdf/include/helper.h: New file.
    * tools/json2rdf/include/json.h: New file.
    * tools/json2rdf/include/list.h: New file.
    * tools/json2rdf/include/ontology.h: New file.
    * tools/json2rdf/include/runtime_configuration.h: New file.
    * tools/json2rdf/include/ui.h: New file.
    * tools/json2rdf/include/yajl_alloc.h: New file.
    * tools/json2rdf/include/yajl_buf.h: New file.
    * tools/json2rdf/include/yajl_bytestack.h: New file.
    * tools/json2rdf/include/yajl_common.h: New file.
    * tools/json2rdf/include/yajl_encode.h: New file.
    * tools/json2rdf/include/yajl_gen.h: New file.
    * tools/json2rdf/include/yajl_lex.h: New file.
    * tools/json2rdf/include/yajl_parse.h: New file.
    * tools/json2rdf/include/yajl_parser.h: New file.
    * tools/json2rdf/src/helper.c: New file.
    * tools/json2rdf/src/json.c: New file.
    * tools/json2rdf/src/list.c: New file.
    * tools/json2rdf/src/main.c: New file.
    * tools/json2rdf/src/ontology.c: New file.
    * tools/json2rdf/src/runtime_configuration.c: New file.
    * tools/json2rdf/src/ui.c: New file.
    * tools/json2rdf/src/yajl.c: New file.
    * tools/json2rdf/src/yajl_alloc.c: New file.
    * tools/json2rdf/src/yajl_buf.c: New file.
    * tools/json2rdf/src/yajl_encode.c: New file.
    * tools/json2rdf/src/yajl_gen.c: New file.
    * tools/json2rdf/src/yajl_lex.c: New file.
    * tools/json2rdf/src/yajl_parser.c: New file.

---
+++ tools/json2rdf/include/ontology.h
@@ -0,0 +1,102 @@
+/* 
+ * Copyright (C) 2019  Roel Janssen <roel@gnu.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ONTOLOGY_H
+#define ONTOLOGY_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <raptor2.h>
+
+/* These string constants can be used to concatenate strings at compile-time. */
+#define URI_W3            "http://www.w3.org"
+#define URI_MASTER        "http://sparqling-genomics.org/" VERSION
+#define URI_ONTOLOGY      URI_MASTER "/json2rdf"
+
+#define STR_PREFIX_MASTER              URI_MASTER "/"
+#define STR_PREFIX_BASE                URI_ONTOLOGY "/"
+#define STR_PREFIX_DYNAMIC_TYPE        URI_ONTOLOGY "/DynamicType/"
+#define STR_PREFIX_ORIGIN              URI_MASTER "/Origin/"
+#define STR_PREFIX_RDF                 URI_W3 "/1999/02/22-rdf-syntax-ns#"
+#define STR_PREFIX_RDFS                URI_W3 "/2000/01/rdf-schema#"
+#define STR_PREFIX_OWL                 URI_W3 "/2002/07/owl#"
+#define STR_PREFIX_XSD                 URI_W3 "/2001/XMLSchema#"
+
+typedef enum
+{
+  PREFIX_BASE = 0,
+  PREFIX_MASTER,
+  PREFIX_ORIGIN,
+  PREFIX_DYNAMIC_TYPE,
+  PREFIX_RDF,
+  PREFIX_RDFS,
+  PREFIX_OWL,
+  PREFIX_XSD
+} ontology_prefix;
+
+typedef enum
+{
+  CLASS_RDF_TYPE = 0,
+  CLASS_ORIGIN,
+} ontology_class;
+
+typedef struct
+{
+  raptor_term **classes;
+  raptor_uri  **prefixes;
+  raptor_uri **xsds;
+  int32_t     classes_length;
+  int32_t     prefixes_length;
+  int32_t     xsds_length;
+} ontology_t;
+
+#define XSD_STRING              0
+#define XSD_INTEGER             1
+#define XSD_FLOAT               2
+#define XSD_BOOLEAN             3
+
+bool ontology_init (ontology_t **ontology_ptr);
+void ontology_free (ontology_t *ontology);
+
+raptor_term* term (int32_t index, char *suffix);
+
+/* The following marcros can be used to construct terms (nodes) and URIs.
+ * These assume 'config.raptor_world', 'config.uris', 'config.ontology',
+ * and 'config.raptor_serializer' exist and have been initialized.
+ */
+#define uri(index, suffix)                                      \
+  raptor_new_uri_relative_to_base (config.raptor_world,         \
+                                   config.uris[index],          \
+                                   str)
+
+/* TODO: Possibly wrap this in raptor_term_copy. */
+#define class(index)                                            \
+  raptor_term_copy (config.ontology->classes[index])
+
+#define literal(str, datatype)                                  \
+  raptor_new_term_from_literal                                  \
+  (config.raptor_world, (unsigned char *)str,                   \
+   config.ontology->xsds[datatype],                             \
+   NULL)
+
+#define register_statement(stmt)                                \
+  raptor_serializer_serialize_statement                         \
+  (config.raptor_serializer, stmt);                             \
+  raptor_free_statement (stmt)
+
+
+#endif  /* ONTOLOGY_H */
