05abaf4ab9bc79dc19c538667e08df313396b975
Roel Janssen <roel@gnu.org>
2020-03-11 14:50:12 +0100

    web: pages login: Simplify.
    * web/www/pages/login.scm: Simplify this page.

--- web/www/pages/login.scm
+++ web/www/pages/login.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2018  Roel Janssen <roel@gnu.org>
+;;; Copyright © 2018, 2019  Roel Janssen <roel@gnu.org>
 ;;;
 ;;; This program is free software: you can redistribute it and/or
 ;;; modify it under the terms of the GNU Affero General Public License
@@ -16,53 +16,31 @@
 
 (define-module (www pages login)
   #:use-module (www pages)
-  #:use-module (www config)
-  #:use-module (www util)
-  #:use-module (www db connections)
-  #:use-module (www db overview)
-  #:use-module (www db sessions)
-  #:use-module (sparql driver)
-  #:use-module (web response)
-  #:use-module (ldap authenticate)
-  #:use-module (ice-9 receive)
-  #:use-module (ice-9 rdelim)
-  #:use-module (srfi srfi-1)
   #:export (page-login))
 
 (define* (page-login request-path #:key (post-data ""))
-  (if (string= post-data "")
-      ;; Handle non-POST requests.
-      (page-empty-template "Log in" request-path
-       `((div (@ (id "login-wrapper"))
-              (form (@ (action "/login") (method "POST"))
-                    (input (@ (type "text")
-                              (class "login-field")
-                              (name "username")
-                              (placeholder "Username")))
-                    (input (@ (type "password")
-                              (class "login-field")
-                              (name "password")
-                              (placeholder "Password")))
-                    (input (@ (class "login-field")
-                              (type "submit")
-                              (value "Log in")))))))
+  (let [(form `(form (@ (action "/login") (method "POST"))
+                     (input (@ (type "text")
+                               (class "login-field")
+                               (name "username")
+                               (placeholder "Username")))
+                     (input (@ (type "password")
+                               (class "login-field")
+                               (name "password")
+                               (placeholder "Password")))
+                     (input (@ (class "login-field")
+                               (type "submit")
+                               (value "Log in")))))]
+    (if (string= post-data "")
+        ;; Handle non-POST requests.
+        (page-empty-template "Log in" request-path
+         `((div (@ (id "login-wrapper")) ,form)))
+
       ;; Handle POST-requests.
       ;; When we get here, the login as failed, because otherwise the
       ;; page handler would've redirected us already.
-      (let ((data (post-data->alist post-data)))
-        (page-empty-template "Log in" request-path
-         `((div (@ (id "login-wrapper"))
-                (div (@ (class "message-box failure"))
-                     (p "Authentication failed."))
-                (form (@ (action "/login") (method "POST"))
-                      (input (@ (type "text")
-                                (class "login-field")
-                                (name "username")
-                                (placeholder "Username")))
-                      (input (@ (type "password")
-                                (class "login-field")
-                                (name "password")
-                                (placeholder "Password")))
-                      (input (@ (class "login-field")
-                                    (type "submit")
-                                    (value "Log in"))))))))))
+      (page-empty-template "Log in" request-path
+       `((div (@ (id "login-wrapper"))
+              (div (@ (class "message-box failure"))
+                   (p "Authentication failed."))
+              ,form))))))
