15e5611d07ad79f46d4faca456ce33c57cbbc2ca
Roel Janssen <roel@gnu.org>
2020-03-20 15:26:42 +0100

    web: db cache: Simplify interface.
    The cache interface increases the changes of cache collisions when the
    codebase size increases.  The remaining functions cache query results,
    and use SHA256 sums to identify a cache.
    
    * web/www/db/cache.scm: Simplify interface.

--- web/www/db/cache.scm
+++ web/www/db/cache.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2018  Roel Janssen <roel@gnu.org>
+;;; Copyright © 2018, 2019, 2020  Roel Janssen <roel@gnu.org>
 ;;;
 ;;; This program is free software: you can redistribute it and/or
 ;;; modify it under the terms of the GNU Affero General Public License
@@ -15,83 +15,19 @@
 ;;; <http://www.gnu.org/licenses/>.
 
 (define-module (www db cache)
-  #:use-module (ice-9 receive)
   #:use-module (ice-9 ftw)
-  #:use-module (sparql driver)
-  #:use-module (sparql util)
-  #:use-module (srfi srfi-1)
   #:use-module (web response)
   #:use-module (www config)
   #:use-module (www db connections)
   #:use-module (www util)
 
-  #:export (cache-clear
-            cache-value
-            cached-value
-
-            cached-query-response
+  #:export (cached-query-response
             cache-query-response
             remove-query-response-cache
 
             cache-response-for-query
-            cached-response-for-query))
-
-;; CACHED-VALUE
-;; ----------------------------------------------------------------------------
-;;
-;; This function retrieves a value from the cache graph.
-;;
-
-(define (cached-value username connection property)
-  (let ((query (format #f "PREFIX web: <http://sparqling-genomics/web/>
-PREFIX : <http://sparqling-genomics/web/cache/>
-
-SELECT ?value FROM <http://~a/sg-cache>
-WHERE {
-  web:cache :~a ?value .
-}" username property)))
-    (catch #t
-      (lambda _
-        (let ((results (query-results->list
-                        (system-sparql-query query)
-                        #t)))
-          (if (null? results)
-              #f
-              results)))
-      (lambda (key . args)
-        #f))))
-
-;; CACHE-VALUE
-;; ----------------------------------------------------------------------------
-;;
-;; This function stores a value into the cache graph.
-;;
-
-(define (cache-value username connection property value)
-  (if (list? value)
-      (map (lambda (item)
-             (cache-value username connection property item))
-           value)
-      (receive (header port)
-          (system-sparql-query
-           (format #f "PREFIX : <http://sparqling-genomics/web/cache/>
-INSERT INTO <http://~a/sg-cache> {
-  <http://sparqling-genomics/web/cache> :~a ~s .
-}" username property value))
-        (= (response-code header) 200))))
-
-;; CACHE-CLEAR
-;; ----------------------------------------------------------------------------
-;;
-;; This function removes all cached items.
-;;
-
-(define (cache-clear username)
-  (receive (header port)
-      (system-sparql-query
-       (format #f "DEFINE sql:log-enable 3
-CLEAR GRAPH <http://~a/sg-cache>" username))
-    (= (response-code header) 200)))
+            cached-response-for-query
+            remove-cached-response-for-query))
 
 ;; WHOLE-QUERY-RESULTS CACHE
 ;; ----------------------------------------------------------------------------
@@ -129,3 +65,10 @@ CLEAR GRAPH <http://~a/sg-cache>" username))
 (define (cached-response-for-query query)
   (let [(sha (string-append "query-" (string->sha256sum query)))]
     (cached-query-response "sgweb" sha)))
+
+(define (remove-cached-response-for-query query)
+  (let* [(sha (string-append "query-" (string->sha256sum query)))
+         (cache-file (cache-location "sgweb" sha))]
+    (when (file-exists? cache-file)
+      (delete-file cache-file))
+    #t))
