7cb226a69b7bc524f3dc5288072cbe6dc5286b4f
Roel Janssen <roel@gnu.org>
2020-03-20 15:39:47 +0100

    web: db exploratory: Implement hierarchical types.
    Other than adapting to the new connections interface, the exploratory
    can deal with hierarchical types.  We extend this interface to support
    the API and user interface code in displaying hierarchical datatypes.
    
    * web/www/db/exploratory.scm: Extend interface with hierarchical types.

--- web/www/db/exploratory.scm
+++ web/www/db/exploratory.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2018  Roel Janssen <roel@gnu.org>
+;;; Copyright © 2018, 2019  Roel Janssen <roel@gnu.org>
 ;;;
 ;;; This program is free software: you can redistribute it and/or
 ;;; modify it under the terms of the GNU Affero General Public License
@@ -15,22 +15,25 @@
 ;;; <http://www.gnu.org/licenses/>.
 
 (define-module (www db exploratory)
-  #:use-module (sparql driver)
   #:use-module (sparql util)
   #:use-module (www db connections)
+  #:use-module (www db projects)
   #:use-module (www db cache)
-  #:use-module (www util)
-  #:use-module (www config)
-  #:use-module (web response)
-  #:use-module (ice-9 receive)
-  #:use-module (ice-9 rdelim)
-  #:use-module (srfi srfi-1)
-  #:use-module (srfi srfi-19)
+  #:use-module ((www config) #:select (default-prefixes
+                                       system-state-graph
+                                       system-sparql-query
+                                       sparql-query-with-connection
+                                       uri->shorthand-uri
+                                       shorthand-uri->uri))
+  #:use-module (ice-9 threads)
   #:use-module (logger)
 
-  #:export (all-graphs-in-connection
+  #:export (all-graphs-in-project
             all-predicates-in-graph
-            all-types-in-graph))
+            all-types
+            all-predicates
+            hierarchical-tree-roots
+            hierarchical-tree-children))
 
 (define (sanitize-graph-name graph)
   (string-map (lambda (x)
@@ -41,119 +44,134 @@
                     #\_ x))
    graph))
 
-(define* (all-graphs-in-connection username connection)
-  (let* [(cache-connection (default-connection username))
-         (property         (generate-id
-                            "available-graph-in-"
-                            (if (connection-name connection)
-                                (connection-name connection)
-                                "")))
-         (cached           (if cache-connection
-                               (cached-value username cache-connection property)
-                               #f))]
+(define (all-graphs-in-project username connection-name project-hash)
+  (let* [(id             (project-id (project-by-hash project-hash)))
+         (query          (string-append
+                          default-prefixes
+                          "SELECT DISTINCT ?graph ?isLocked ?connectionName "
+                          "FROM <" system-state-graph "> "
+                          "WHERE {"
+                          " <" id "> sg:hasAssignedGraph ?graph . "
+                          " ?graph sg:inConnection "
+                          (if connection-name
+                              (string-append
+                               "\"" connection-name "\"^^xsd:string . ")
+                              "?connectionName .")
+                          " OPTIONAL { ?graph sg:isLocked ?lockState . }"
+                          " BIND(IF(BOUND(?lockState), ?lockState,"
+                          " \"false\"^^xsd:boolean) AS ?isLocked) "
+                          (if connection-name
+                              (string-append
+                               "BIND (\"" connection-name "\" AS ?connectionName)")
+                              "")
+                          " }"))]
+    (query-results->alist
+     (system-sparql-query query))))
+
+(define* (all-predicates username connection project-hash token
+                         #:key (graph #f) (type #f))
+  (catch #t
+    (lambda _
+      (let* [(query
+              (string-append
+               default-prefixes
+               "SELECT DISTINCT ?predicate WHERE { "
+               (if graph
+                   (string-append "GRAPH <" (shorthand-uri->uri graph) "> { ")
+                   "")
+               (if type
+                   (string-append "?s rdf:type <" (shorthand-uri->uri type) "> . ")
+                   "")
+               "?s ?predicate ?o ."
+               " OPTIONAL { ?o rdf:type ?o_type . }"
+               " OPTIONAL { ?predicate sg:isSystemProperty ?systype . }"
+               " BIND ((IF (isLiteral(?o), datatype(?o), ?o_type) AS ?datatype))"
+               " FILTER (!BOUND(?systype) OR ?systype != 1)"
+               (if graph "}" "")
+               "FILTER (?predicate != rdf:type) }"))
+             (cached (cached-response-for-query query))]
+        (if cached cached
+            (let [(response (map (lambda (item)
+                                   `((,(car (car item)) . ,(uri->shorthand-uri
+                                                            (cdr (car item))))))
+                                 (query-results->alist
+                                  (sparql-query-with-connection
+                                   connection query token project-hash))))]
+              (call-with-new-thread
+               (lambda _
+                 (cache-response-for-query query response)))
+              response))))
+    (lambda (key . args)
+      (log-error "all-predicates"
+                 "Unknown exception thrown: ~a: ~s" key args)
+      '())))
+
+(define* (all-types username connection token project-hash)
+  (let* [(query (string-append
+                 default-prefixes
+                 "SELECT DISTINCT ?type WHERE { ?s rdf:type ?type . }"))
+         (cached           (cached-response-for-query query))]
     (cond
-     [cached (sort (map car cached) string<)]
+     [cached cached]
      [connection
       (catch #t
         (lambda _
-          (let ((result (apply append
-                               (query-results->list
-                                (sparql-query
-                                 "SELECT DISTINCT ?graph WHERE { GRAPH ?graph { ?s ?p ?o } }"
-                                 #:uri           (connection-uri connection)
-                                 #:store-backend (connection-backend connection)
-                                 #:digest-auth   (if (and (connection-username connection)
-                                                          (connection-password connection))
-                                                     (string-append
-                                                      (connection-username connection) ":"
-                                                      (connection-password connection))
-                                                     #f))
-                                #t))))
-            (cache-value username cache-connection property result)
-            (sort result string<)))
+          (let [(result (sort
+                         (apply append
+                                (query-results->list
+                                 (sparql-query-with-connection
+                                  connection query token project-hash)
+                                 #t)) string<))]
+            (cache-response-for-query query result)
+            result))
         (lambda (key . args)
-          #f))]
-     [else #f])))
+          (log-error "all-types"
+                     "Unknown exception thrown in ~a: ~a" key args) '()))]
+     [else '()])))
 
-(define (all-predicates-in-graph username graph connection type)
+;;
+;; HIERARCHICAL TREE BUILDING
+;; ----------------------------------------------------------------------------
+;;
+
+(define (hierarchical-tree-roots connection graph-uri token project-hash)
+  "Returns data types that have no parents for GRAPH-URI in CONNECTION."
+  (let* [(query
+          (string-append default-prefixes "\n"
+                         "SELECT DISTINCT ?type { GRAPH <"
+                         (shorthand-uri->uri graph-uri) "> {"
+                         " ?s rdf:type ?type ."
+                         " OPTIONAL { ?s sg:isPartOf ?parent . }"
+                         " FILTER (! BOUND(?parent))"
+                         " FILTER (?type != xml2rdf:XmlAttribute) } }"))
+         (tree-roots
+          (map uri->shorthand-uri
+               (apply append
+                      (query-results->list
+                       (sparql-query-with-connection
+                        connection query token project-hash) #t))))]
+    tree-roots))
+
+(define (hierarchical-tree-children connection token project-hash graph-uri
+                                    tree-root)
+  "Returns the direct children of TREE-ROOT for GRAPH-URI in CONNECTION."
   (catch #t
     (lambda _
-      (let* [(cache-connection (default-connection username))
-             (property    (generate-id
-                           "all-predicates-in-graph-"
-                           (if (connection-name connection)
-                               (connection-name connection)
-                               "")
-                           (if graph (sanitize-graph-name graph) "")
-                           (if type (sanitize-graph-name type)   "")))
-             (cached       (if cache-connection
-                               (cached-value username cache-connection property)
-                               #f))]
-        (cond
-         [cached (sort (map car cached) string<)]
-         [connection
-          (let [(result (map uri->shorthand-uri
-                             (apply append
-                                    (query-results->list
-                                     (sparql-query
-                                      (string-append
-                                       "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-                                       "SELECT DISTINCT ?predicate WHERE { GRAPH <"
-                                       (shorthand-uri->uri graph) "> { ?s rdf:type <"
-                                       (shorthand-uri->uri type) "> . ?s ?predicate ?o } }")
-                                      #:uri           (connection-uri connection)
-                                      #:store-backend (connection-backend connection)
-                                      #:digest-auth   (if (and (connection-username connection)
-                                                               (connection-password connection))
-                                                          (string-append
-                                                           (connection-username connection) ":"
-                                                           (connection-password connection))
-                                                          #f)) #t))))]
-            (cache-value username cache-connection property result)
-            (sort result string<))]
-         [else #f])))
+      (let* [(query
+              (string-append
+               default-prefixes
+               "\n"
+               "SELECT DISTINCT ?type { GRAPH <"
+               (shorthand-uri->uri graph-uri) "> {"
+               " ?s rdf:type <" (shorthand-uri->uri tree-root) "> ."
+               " ?c sg:isPartOf ?s ; rdf:type ?type . } }"))
+             (children
+              (map uri->shorthand-uri
+                   (apply append
+                          (query-results->list
+                           (sparql-query-with-connection connection query token
+                                                         project-hash) #t))))]
+        children))
     (lambda (key . args)
-      (log-error "all-predicates-in-graph"
-                 "Unknown exception thrown: ~a: ~a" key args)
-      '())))
-
-(define (all-types-in-graph username graph connection)
-  (catch #t
-    (lambda _
-      (let* [(cache-connection (default-connection username))
-             (property         (generate-id
-                                "all-types-in-graph-"
-                                (if (connection-name connection)
-                                    (connection-name connection)
-                                    "")
-                                (if graph (sanitize-graph-name graph) "")))
-             (cached           (if cache-connection
-                                   (cached-value username cache-connection property)
-                                   #f))]
-        (cond
-         [cached (sort (map car cached) string<)]
-         [connection
-          (let [(result
-                 (map uri->shorthand-uri
-                      (apply append
-                             (query-results->list
-                              (sparql-query
-                               (string-append
-                                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-                                "SELECT DISTINCT ?type WHERE { GRAPH <" (shorthand-uri->uri graph) "> "
-                                "{ ?s rdf:type ?type } }")
-                               #:uri           (connection-uri connection)
-                               #:store-backend (connection-backend connection)
-                               #:digest-auth   (if (and (connection-username connection)
-                                                        (connection-password connection))
-                                                   (string-append
-                                                    (connection-username connection) ":"
-                                                    (connection-password connection))
-                                                   #f)) #t))))]
-            (cache-value username cache-connection property result)
-            (sort result string<))]
-         [else #f])))
-    (lambda (key . args)
-      (log-error "all-types-in-graph"
-                 "Unknown exception thrown: ~a: ~a" key args)
+      (log-error "hierarchical-tree-children" "Thrown: ~a: ~s" key args)
       '())))
