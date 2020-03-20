d060da486c564716d755ca88ce45a088e7682a14
Roel Janssen <roel@gnu.org>
2020-03-20 15:43:10 +0100

    web: db prompt: Extend interface for multi-prompts.
    This interface enables users to create multiple prompts.
    
    * web/www/db/prompt.scm: Extend interface for multi-prompts.

--- web/www/db/prompt.scm
+++ web/www/db/prompt.scm
@@ -16,22 +16,26 @@
 
 (define-module (www db prompt)
   #:use-module (ice-9 receive)
-  #:use-module (sparql driver)
   #:use-module (sparql util)
-  #:use-module (srfi srfi-1)
+  #:use-module (rnrs io ports)
   #:use-module (web response)
   #:use-module (www config)
   #:use-module (www db connections)
-  #:use-module (www db exploratory)
-  #:use-module (www db sessions)
   #:use-module (www util)
   #:use-module (logger)
 
-  #:export (prompt-insert-triplet
-            prompt-remove-triplet
+  #:export (prompt-start
+            prompt-delete
+            prompt-add-triplet
+            prompt-delete-triplet
+            prompt-with-tag
+            prompt-commit
+
             prompt-get-triplets
             prompt-clear-triplets
-            prompt-save-session))
+            prompt-save-session
+
+            prompts-by-user))
 
 (define (restore-uri input)
   (cond
@@ -48,147 +52,198 @@
         input
         (string-append "\"" input "\""))]))
 
-;; PROMPT-INSERT-TRIPLET
-;; ----------------------------------------------------------------------------
-;;
-;; This function stores a value into the cache graph.
-;;
+(define (rdf-token-type token)
+  (cond
+   [(string-prefix? "<" token)    'IRI]
+   [(is-shorthand-uri? token)     'IRI]
+   [(string-prefix? "\"" token)   'literal]
+   [else 'literal]))
 
-(define (prompt-insert-triplet username subject predicate object)
-  (let [(connection (default-connection username))]
-    (catch #t
-      (lambda _
-        (let [(query (format #f "~a
-INSERT INTO <http://~a/sg-prompt> {
-  ~a ~a ~a
-}" default-prefixes username
- (restore-uri subject)
- (restore-uri predicate)
- (restore-uri object)))]
-          (receive (header port)
-              (sparql-query query
-                            #:uri (connection-uri connection)
-                            #:store-backend
-                            (connection-backend connection)
-                            #:digest-auth
-                            (if (and (connection-username connection)
-                                     (connection-password connection))
-                                (string-append
-                                 (connection-username connection) ":"
-                                 (connection-password connection))
-                                #f))
-            (= (response-code header) 200))))
-      (lambda (key . args)
-        (log-error "prompt-insert-triplet"
-                   "Uknown exception thrown: ~a" key)
-        '()))))
+(define* (prompt-start username #:key (tag #f))
+  (let* [(timestamp (strftime "%Y-%m-%dT%H:%M:%SZ"
+                              (gmtime (current-time))))
+         (prompt-id (generate-id "prompt-" username "-" timestamp))
+         (query (string-append
+                 default-prefixes
+                 "INSERT INTO <" system-state-graph "> {"
+                 " <prompt://" prompt-id "> rdf:type <prompt://session> ;"
+                 " sg:owner agent:" username " ;"
+                 (if tag
+                     (string-append " sg:promptTag \"" tag "\"^^xsd:string ;")
+                     "")
+                 " dcterms:created \"" timestamp "\"^^xsd:dateTime . }"))]
+    (receive (header port)
+        (system-sparql-query query)
+      (if (= (response-code header) 200)
+          prompt-id
+          #f))))
 
-;; PROMPT-REMOVE-TRIPLET
-;; ----------------------------------------------------------------------------
-;;
-;; This function removes a triplet from the prompt session's graph.
-;;
+(define* (prompt-with-tag tag username #:key (create? #f))
+  (let* [(query (string-append
+                 default-prefixes
+                 "SELECT DISTINCT (STRAFTER(STR(?id), 'prompt://') AS ?id)"
+                 " WHERE { GRAPH <" system-state-graph "> {"
+                 " ?id rdf:type <prompt://session> ;"
+                 " sg:owner agent:" username " ;"
+                 " sg:promptTag \"" tag "\"^^xsd:string . } } LIMIT 1"))
+         (results (query-results->alist (system-sparql-query query)))]
+    (if (null? results)
+        (if create?
+            (prompt-start username #:tag tag)
+            #f)
+        (assoc-ref (car results) "id"))))
 
-(define (prompt-remove-triplet username subject predicate object)
-  (let [(connection (default-connection username))]
-    (catch #t
-      (lambda _
-        (let [(query (format #f "~a
-DELETE DATA {
-  GRAPH <http://~a/sg-prompt> {
-    ~a ~a ~a
-  }
-}
-" default-prefixes
-  username
-  (restore-uri subject)
-  (restore-uri predicate)
-  (restore-uri object)))]
-          (receive (header port)
-              (user-sparql-query username query)
-            (= (response-code header) 200))))
-      (lambda (key . args)
-        (log-error "prompt-remove-triplet"
-                   "Unknown exception thrown ~a: ~a"
-                   key args)))))
+(define (prompt-delete prompt-id username)
+  (catch #t
+    (lambda _
+      (let* [(query1 (string-append
+                      default-prefixes
+                      "DELETE { GRAPH <" system-state-graph "> {"
+                      " ?prompt_id ?p ?o . } } "
+                      "WHERE { GRAPH <" system-state-graph "> { "
+                      " ?prompt_id rdf:type <prompt://session> ;"
+                      " sg:owner agent:" username " ; ?p ?o ."
+                      " FILTER (?prompt_id = <prompt://" prompt-id ">) "
+                      "} }"))
+             (query2 (string-append
+                      default-prefixes
+                      "DELETE { GRAPH <" system-state-graph "> {"
+                      " ?triplet_id ?p ?o . } } "
+                      "WHERE { GRAPH <" system-state-graph "> { "
+                      " ?triplet_id sg:isPartOf ?prompt_id ; ?p ?o ."
+                      " FILTER (?prompt_id = <prompt://" prompt-id ">) "
+                      "} }"))]
+        (and
+         (receive (header port)
+             (system-sparql-query query1)
+           (= (response-code header) 200))
+         (receive (header port)
+             (system-sparql-query query2)
+           (= (response-code header) 200)))))
+    (lambda (key . args)
+      (log-error "prompt-delete"
+                 "Uknown exception thrown: ~a" key)
+      #f)))
 
-;; PROMPT-GET-TRIPLETS
-;; ----------------------------------------------------------------------------
-
-(define (prompt-get-triplets username)
-  (let [(connection (default-connection username))]
-    (catch #t
-      (lambda _
-        (let [(results
-               (query-results->list
-                (sparql-query
-                 (format #f "SELECT DISTINCT ?s ?p ?o WHERE { GRAPH <~a> { ?s ?p ?o } }"
-                         (string-append "http://" username "/sg-prompt"))
-                 #:uri           (connection-uri connection)
-                 #:store-backend (connection-backend connection)
-                 #:digest-auth   (if (and (connection-username connection)
-                                          (connection-password connection))
-                                     (string-append
-                                      (connection-username connection) ":"
-                                      (connection-password connection))
-                                     #f))
-                #t))]
-          (if results
-              results
-              '())))
-      (lambda (key . args)
-        (log-error "prompt-get-triplets"
-                   "Unknown exception thrown ~a"
-                   args)
-        '()))))
-
-;; PROMPT-CLEAR-TRIPLETS
-;; ----------------------------------------------------------------------------
-
-(define (prompt-clear-triplets username)
-  (let [(connection (default-connection username))]
-    (catch #t
-      (lambda _
-        (query-results->list
-         (sparql-query
-          (string-append "CLEAR GRAPH <http://" username "/sg-prompt>")
-          #:uri           (connection-uri connection)
-          #:store-backend (connection-backend connection)
-          #:digest-auth   (if (and (connection-username connection)
-                                   (connection-password connection))
-                              (string-append
-                               (connection-username connection) ":"
-                               (connection-password connection))
-                              #f))
-         #t))
-      (lambda (key . args)
-        (log-error "prompt-clear-triplets"
-                   "Unknown exception thrown ~a: ~a"
-                   key args)
-        '()))))
-
-(define (prompt-save-session username graph)
-  (let [(connection (default-connection username))]
-    (catch #t
-      (lambda _
+(define (prompt-delete-triplet triplet-id username)
+  (catch #t
+    (lambda _
+      (let* [(query (string-append
+                    default-prefixes
+                    "DELETE { GRAPH <" system-state-graph "> {"
+                    " ?triplet_id ?p ?o . } } "
+                    "WHERE { GRAPH <" system-state-graph "> { "
+                    " ?prompt rdf:type <prompt://session> ;"
+                    " sg:owner agent:" username " ."
+                    " ?triplet_id sg:isPartOf ?prompt ; ?p ?o . "
+                    " FILTER (?triplet_id = <triplet://" triplet-id ">)"
+                    "} }"))]
         (receive (header port)
-            (sparql-query
-             (string-append "INSERT { GRAPH <" graph "> { ?s ?p ?o } } "
-                            "WHERE { GRAPH <http://" username "/sg-prompt> { "
-                            "?s ?p ?o } }")
-             #:uri           (connection-uri connection)
-             #:store-backend (connection-backend connection)
-             #:digest-auth   (if (and (connection-username connection)
-                                      (connection-password connection))
-                                 (string-append
-                                  (connection-username connection) ":"
-                                  (connection-password connection))
-                                 #f))
+            (system-sparql-query query)
+          (= (response-code header) 200))))
+    (lambda (key . args)
+      (log-error "prompt-delete-triplet"
+                 "Uknown exception thrown: ~a" key)
+      #f)))
+
+
+(define (prompt-add-triplet prompt-id username subject predicate object)
+  (catch #t
+    (lambda _
+      (let* [(triplet-id (generate-id "triplet-" subject predicate object))
+             (query (string-append
+                    default-prefixes
+                    "INSERT INTO <" system-state-graph "> {"
+                    " <prompt://" prompt-id "> rdf:type <prompt://session> ;"
+                    "                          sg:owner agent:" username " ."
+                    " <triplet://" triplet-id "> sg:isPartOf <prompt://" prompt-id "> ;"
+                    "                            <triplet://subject> " subject " ;"
+                    "                            <triplet://predicate> " predicate " ;"
+                    "                            <triplet://object> " object " . "
+                    "}"))]
+        (receive (header port)
+            (system-sparql-query query)
           (if (= (response-code header) 200)
-              (prompt-clear-triplets username)
-              #f)))
+              triplet-id
+              #f))))
+    (lambda (key . args)
+      (log-error "prompt-add-triplet" "Uknown exception thrown: ~a" key)
+      #f)))
+
+(define (prompt-get-triplets prompt-id username)
+  (catch #t
+    (lambda _
+      (let* [(query (string-append
+                     default-prefixes
+                     "SELECT (STRAFTER(STR(?triplet_id), 'triplet://') AS ?triplet_id) "
+                     "?subject ?predicate ?object WHERE {"
+                     " GRAPH <" system-state-graph "> {"
+                     " <prompt://" prompt-id "> rdf:type <prompt://session> ;"
+                     "                          sg:owner agent:" username " ."
+                     " ?triplet_id sg:isPartOf <prompt://" prompt-id "> ;"
+                     "             <triplet://subject>   ?subject ;"
+                     "             <triplet://predicate> ?predicate ;"
+                     "             <triplet://object>    ?object . "
+                     "} }"))
+             (results (query-results->alist (system-sparql-query query)))]
+        results))
+    (lambda (key . args)
+      (log-error "prompt-get-triplets" "Uknown exception thrown: ~a" key)
+      #f)))
+
+;; PROMPT-COMMIT
+;; ----------------------------------------------------------------------------
+
+(define (prompt-commit prompt-id graph username token project-hash)
+  (let [(connection (default-connection username))]
+    (catch #t
+      (lambda _
+        (let [(query (string-append
+              default-prefixes
+              "INSERT { GRAPH <" graph "> { ?s ?p ?o . } } "
+              "WHERE { GRAPH <" system-state-graph "> { "
+              "?triplet sg:isPartOf <prompt://" prompt-id "> ; "
+              "<triplet://subject> ?s ; "
+              "<triplet://predicate> ?p ; "
+              "<triplet://object> ?o . } }"))]
+          (receive (header port)
+              (system-sparql-query query)
+            (cond
+             [(= (response-code header) 200)
+              (prompt-delete prompt-id username)]
+             [(= (response-code header) 500)
+              (log-error "prompt-commit" "Server responded 500:~%~a~%"
+                         (get-string-all port))]
+             [else
+              (log-error "prompt-commit" "Unexpected return state: ~a"
+                         (response-code header))]))))
       (lambda (key . args)
-        (log-error "prompt-save-session"
-                   "Unknown exception thrown ~a: ~a"
-                   key args)
-        '()))))
+        (log-error "prompt-commit" "Unknown exception thrown ~a: ~a" key args)
+        #f))))
+
+;; PROMPTS-BY-USER
+;; ----------------------------------------------------------------------------
+
+(define (prompts-by-user username)
+  (let* [(query (string-append
+                 default-prefixes
+                 "SELECT (STRAFTER(STR(?prompt), \"prompt://\") AS ?id)"
+                 " ?tag ?creationDate (COUNT(?triplet) AS ?triplets) "
+                 "WHERE { GRAPH <" system-state-graph "> {"
+                 " ?prompt rdf:type <prompt://session> ."
+                 " OPTIONAL { ?prompt sg:promptTag ?tag . }"
+                 " ?prompt sg:owner agent:" username " ."
+                 " ?prompt dcterms:created ?creationDate ."
+                 " OPTIONAL { ?triplet sg:isPartOf ?prompt . }"
+                 "} }"))]
+    (receive (header port)
+        (system-sparql-query query)
+      (if (= (response-code header) 200)
+          (query-results->alist
+           (values header port))
+          (begin
+            (log-error "prompts-by-user"
+                       "Unable to retrieve prompts for ~s due to a ~a response."
+                       username
+                       (response-code header))
+            #f)))))
