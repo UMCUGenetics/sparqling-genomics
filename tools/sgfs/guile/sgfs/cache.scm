19ba256faf17d77e35cd97044a63bb0975e03a45
Roel Janssen <roel@gnu.org>
2020-08-27 16:34:44 +0200

    tools: sgfs: Add ‘(sgfs cache)’.
    This commit introduces the ‘(sgfs cache)’ module that can be used to
    store all retrieved information through API calls.  By doing so, we
    trade network traffic for memory.
    
    * tools/sgfs/guile/Makefile.am: Register ‘sgfs/cache.scm’.
    * tools/sgfs/guile/sgfs/cache.scm: New file.
    * tools/sgfs/src/sgfs.c.in: Create mutexes for the cache module.

---
+++ tools/sgfs/guile/sgfs/cache.scm
@@ -0,0 +1,48 @@
+;;; Copyright © 2020  Roel Janssen <roel@gnu.org>
+;;;
+;;; This program is free software: you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation, either version 3 of
+;;; the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (sgfs cache)
+  #:use-module (ice-9 threads)
+
+  #:export (add-project-to-cache
+            add-query-to-cache
+
+            projects
+            queries
+
+            set-projects-lock!
+            set-queries-lock!))
+
+(define %projects-lock '())
+(define %queries-lock '())
+
+(define %projects '())
+(define %queries  '())
+
+(define (add-project-to-cache project)
+  (lock-mutex %cache-projects-mutex)
+  (set! %projects (cons project %projects))
+  (unlock-mutex %cache-projects-mutex))
+
+(define (add-query-to-cache query)
+  (lock-mutex %cache-queries-mutex)
+  (set! %queries (cons query %queries))
+  (unlock-mutex %cache-queries-mutex))
+
+(define (projects) %projects)
+(define (queries)  %queries)
+
+(define (set-projects-lock! lock) (set! %projects-lock lock))
+(define (set-queries-lock! lock)  (set! %queries-lock lock))
