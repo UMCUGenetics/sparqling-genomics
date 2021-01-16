272fa0c0e635ee7d6d27aa15103aca63b48b86fe
Roel Janssen <roel@gnu.org>
2021-01-17 00:15:06 +0100

    web: Update to guile-json 4.5.1.
    * web/Makefile.am: Register ‘record.scm’.
    * web/json.scm: Update to guile-json 4.5.1.
    * web/json/builder.scm: Likewise.
    * web/json/parser.scm: Likewise.
    * web/json/record.scm: New file.
    * web/json/syntax.scm: Remove file.

--- web/json.scm
+++ web/json.scm
@@ -1,23 +1,21 @@
 ;;; (json) --- Guile JSON implementation.
 
-;; Copyright (C) 2013 Aleix Conchillo Flaque <aconchillo@gmail.com>
+;; Copyright (C) 2013-2020 Aleix Conchillo Flaque <aconchillo@gmail.com>
 ;;
 ;; This file is part of guile-json.
 ;;
-;; guile-json is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU Lesser General Public
-;; License as published by the Free Software Foundation; either
-;; version 3 of the License, or (at your option) any later version.
+;; guile-json is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3 of the License, or
+;; (at your option) any later version.
 ;;
-;; guile-json is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; Lesser General Public License for more details.
+;; guile-json is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
 ;;
-;; You should have received a copy of the GNU Lesser General Public
-;; License along with guile-json; if not, write to the Free Software
-;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-;; 02110-1301 USA
+;; You should have received a copy of the GNU General Public License
+;; along with guile-json. If not, see https://www.gnu.org/licenses/.
 
 ;;; Commentary:
 
@@ -28,7 +26,7 @@
 (define-module (json)
   #:use-module (json builder)
   #:use-module (json parser)
-  #:use-module (json syntax))
+  #:use-module (json record))
 
 (define-syntax re-export-modules
   (syntax-rules ()
@@ -40,6 +38,6 @@
 
 (re-export-modules (json builder)
                    (json parser)
-                   (json syntax))
+                   (json record))
 
 ;;; (json) ends here
